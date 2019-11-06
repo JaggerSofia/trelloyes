a3565f0f74a9ffecb8ffb6ccecc6fe29100c2e0e
Victoria Moore <victoriamoore563@gmail.com>
2019-11-05 21:43:49 -0800

    rewrote entire f 2 make clas App rather than funct

--- src/App.js
+++ src/App.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css';
 import './list.css';
 import './card.css';
 import './store.js';
 import List from './list.js';
 
+class App extends Component {
+  static defaultProps ={
+    store: {
+      lists: [],
+      allCards: {},
+    }
+  };
 
-function App(props) {
-  const listsArray=props.store.lists;
-  const allCards=props.store.allCards;
-  const listComponents=listsArray.map(item => <List key={item.id} header={item.header} cards={item.cardIds.map(
-    item => allCards[item])}/>)
-
-  return (
-    <main className='App'>
-      <div className='App-header'>
+  render() {
+    const  { store } = this.props
+    return(
+      <main className='App'>
+        <header className='App-header'>
           <h1>Trelloyes!</h1>
-      </div>
-      <div className='App-list'>
-        {listComponents}
-      </div>
-    </main>
-  );
+        </header>
+        <div className='App-list'>
+          {store.lists.map(list => (
+            <List
+              key={list.id}
+              header={list.header}
+              cards={list.cardIds.map(id => store.allCards[id])}
+            />
+          ))}
+        </div>
+      </main>
+    )
+  }
 }
+// function App(props) {
+//   const listsArray=props.store.lists;
+//   const allCards=props.store.allCards;
+//   const listComponents=listsArray.map(item => <List key={item.id} header={item.header} cards={item.cardIds.map(
+//     item => allCards[item])}/>)
+
+//   return (
+//     <main className='App'>
+//       <div className='App-header'>
+//           <h1>Trelloyes!</h1>
+//       </div>
+//       <div className='App-list'>
+//         {listComponents}
+//       </div>
+//     </main>
+//   );
+// }
 
 export default App;
