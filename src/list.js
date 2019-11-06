fbb689ef185e0ebabcbc01d883d7f8b0a10feef1
Victoria Moore <victoriamoore563@gmail.com>
2019-11-05 21:44:52 -0800

    rewrote entire file to make test work

--- src/list.js
+++ src/list.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import Card from './card.js'
+import './list.css';
 
-function List(props) {
+export default function List(props) {
     console.log()
-    const cardsArray=props.cards;
-    const cardsComponents=cardsArray.map(card => <Card key={card.id} title={card.title} content={card.content}/>)
+    // const cardsArray=props.cards;
+    // const cardsComponents=cardsArray.map(card => <Card key={card.id} title={card.title} content={card.content}/>)
     return (
     <section className='List'>
         <header className='List-header'>
             <h2>{props.header}</h2>
         </header>
         <div className='List-cards'>
-        {cardsComponents} 
+        {props.cards.map((card) =>
+            <Card
+                key={card.id}
+                title={card.title}
+                content={card.content}
+            />
+            )} 
         <button type='button' className='List-add-button'>
             + Add Random Card
         </button>
@@ -19,4 +26,10 @@ function List(props) {
     </section>)
 }
 
-export default List
+// export default List
+
+List.defaultProps = {
+    key:" ",
+    header: " ",
+    cards: []
+}
