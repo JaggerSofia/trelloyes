03a9d03958b1c9de75ef4c7413d5989f1933f8b4
Victoria Moore <victoriamoore563@gmail.com>
2019-11-12 21:14:12 -0800

    created callback prop 2 make delete & random butW*

--- src/card.js
+++ src/card.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import './card.css';
 
 
 function Card(props) {
     return (
     <div className='Card'>
-        <button type='button'>delete</button>
+        <button type='button' onClick={() => props.onDeleteCard(props.id)}>delete</button>
         <h3>{props.title}</h3>
         <p>{props.content}</p>
     </div>
     )
+}
+
+Card.propTypes ={
+    onDeleteCard: () => {}
 }
 
 export default Card;
